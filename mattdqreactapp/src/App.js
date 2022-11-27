e62bcd22dca8042c1158dd8918524da5b447e8d0
Matheus Duque <antunesdq@gmail.com>
2022-11-27 09:31:15 +0000

    v0.0.2 - Loading page done!

--- mattdqreactapp/src/App.js
+++ mattdqreactapp/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import loading from './gifs/loading.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-loading">
+          <p> 
+            Loading....
+          </p>
+        </div>
+        <img src={loading} className="App-logo" alt="logo" />
+     </header>
     </div>
   );
 }
