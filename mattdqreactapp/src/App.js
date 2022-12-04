cd7876f3e9dac120492312d0dd55dad882f0def7
Matheus Duque <antunesdq@gmail.com>
2022-12-04 16:41:12 +0000

    v0.0.6 - Switched to react-router-dom. 🤓

--- mattdqreactapp/src/App.js
+++ mattdqreactapp/src/App.js
@@ -5,32 +5,19 @@ import Contact from './pages/Contact'
 import Portfolio from './pages/Portfolio'
 import Blog from './pages/Blog'
 import Home from './pages/Home'
+import { Route, Routes} from 'react-router-dom'
 
 function App() {
-    let Component 
-    switch (window.location.pathname) {
-        case '/home':
-            Component = Home
-            break
-        case '/about':
-            Component = About
-            break
-        case '/contact':
-            Component = Contact
-            break
-        case '/portfolio':
-            Component = Portfolio
-            break
-        case '/blog':
-            Component = Blog
-            break
-        default:
-            break
-    }
     return (
         <div className="App">
             <Navbar />
-            <Component />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/blog" element={<Blog />} />
+                </Routes>
         </div>
     );
 }
--- mattdqreactapp/src/Components/navbar.js
+++ mattdqreactapp/src/Components/navbar.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <nav className="App-nav">
-                <div className="App-subheader">
-                    <a href="/home" className="title">Matheus Duque</a>
-                    <a href="/home" className="sub-title">BackEnd Developer</a>
-                </div>
-                <ul>
-                    <li class="portfolio">
-                        <a href="/portfolio">Portfolio</a>
-                    </li>
-                    <li class="blog">
-                        <a href="/blog">Blog</a>
-                    </li>
-                    <li class="contact">
-                        <a href="/contact">Contact</a>
-                    </li>
-                    <li class="about">
-                        <a href="/about">About</a>
-                    </li>
-                    <li clas="lang">
-                        <button class="lang-button">EN</button>
-                        <button class="lang-button">PT</button>
-                        <button class="lang-button">SE</button>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+export default function Navbar() {
+    return (
+        <nav className="App-nav">
+            <div className="App-subheader">
+                <Link to="/" className="title">Matheus Duque</Link>
+                <Link to="/" className="sub-title">BackEnd Developer</Link>
+            </div>
+            <ul>
+                <li class="portfolio">
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li class="blog">
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li class="contact">
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li class="about">
+                    <Link to="/about">About</Link>
+                </li>
+                <li clas="lang">
+                    <button class="lang-button">EN</button>
+                    <button class="lang-button">PT</button>
+                    <button class="lang-button">SE</button>
+                </li>
+            </ul>
+        </nav>
+    );
 }
-
-export default Navbar;
